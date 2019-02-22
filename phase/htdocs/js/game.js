3be5ce2f7257a6e91e85ac48347c3ca6a93c25ff
Rúben Santos <1161391@isep.ipp.pt>
2019-02-22 23:19:24 +0000

    Player + Earth

--- phase/htdocs/js/game.js
+++ phase/htdocs/js/game.js
@@ -9,7 +9,7 @@ var config = {
         default: 'arcade',
         arcade: {
             gravity: {
-                y: 200
+                y: 0
             }
         }
     },
@@ -24,22 +24,53 @@ var game = new Phaser.Game(config);
 
 var homePlanet;
 var sun;
+var earth;
+var cursors;
+var raio;
+var rotation = 0.3;
+var angle = Math.PI / 2;
 
-var rotation = 0.5;
-
-var rotation;
+var rotation;   
 
 function preload() {
     this.load.image('sky', 'assets/sky.png');
     this.load.image('mars', 'assets/mars.png');
+    this.load.image('earth', 'assets/earth.png');
+    this.load.spritesheet('player',
+				'assets/dude.png', {
+					frameWidth: 32,
+					frameHeight: 48
+				});
 }
 
 function create() {
     this.add.image(400, 300, 'sky');
-    this.add.image()
-    homePlanet = this.add.image(500, 500, 'mars');
+    earth = this.add.sprite(config.width/2, config.height/2, 'earth');
+    earth.scaleX = 0.4;
+    earth.scaleY = 0.4;
+    raio = ((earth.height * earth.scaleY)/2);
+    homePlanet = this.add.image(650, 520, 'mars');
+    player = this.add.sprite(earth.x, earth.y - raio, 'player');
+    
+    player.y -= player.height/2;
+
+    player.anchor.setTo(earth.x, earth.y);
+    player.setCollideWorldBounds(true);
+    cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update() {
     homePlanet.angle += rotation;
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+        
+        // player.anims.play('left', true);
+    } else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+        // player.anims.play('right', true);
+    } else {
+        player.setVelocityX(0);
+        // player.anims.play('turn');
+    }
+    
 }
