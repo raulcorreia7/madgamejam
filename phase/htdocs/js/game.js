62d8f7e953f39d50600e30358091b376f2751e7b
Raul Correia <raulcorreia7@gmail.com>
2019-02-23 00:11:03 +0000

    sky fix

--- phase/htdocs/js/game.js
+++ phase/htdocs/js/game.js
@@ -25,93 +25,58 @@ var config = {
 
 var game = new Phaser.Game(config);
 
-var homePlanet;
+
+//The sun
 var sun;
-<<<<<<< HEAD
+//Earth
 var earth;
+//Sky
+var sky;
 var cursors;
 var raio;
 var rotation = 0.3;
 var angle = Math.PI / 2;
 
-var rotation;   
-=======
-var sky;
-
-var rotation = 0.25;
-
-var sun_properties = {
-    radius: 0,
-    angle: 0
-
-}
->>>>>>> b1a52344ce952db4a85eacc1f35ca6e1e317e9a7
+var rotation;
 
 function preload() {
     this.load.image('sky', 'assets/sky.png');
-    this.load.image('mars', 'assets/mars.png');
-<<<<<<< HEAD
     this.load.image('earth', 'assets/earth.png');
     this.load.spritesheet('player',
-				'assets/dude.png', {
-					frameWidth: 32,
-					frameHeight: 48
-				});
-}
-
-function create() {
-    this.add.image(400, 300, 'sky');
-    earth = this.add.sprite(config.width/2, config.height/2, 'earth');
-    earth.scaleX = 0.4;
-    earth.scaleY = 0.4;
-    raio = ((earth.height * earth.scaleY)/2);
-    homePlanet = this.add.image(650, 520, 'mars');
-    player = this.add.sprite(earth.x, earth.y - raio, 'player');
-    
-    player.y -= player.height/2;
-
-    player.anchor.setTo(earth.x, earth.y);
-    player.setCollideWorldBounds(true);
-    cursors = this.input.keyboard.createCursorKeys();
-}
-
-function update() {
-    homePlanet.angle += rotation;
-    if (cursors.left.isDown) {
-        player.setVelocityX(-160);
-        
-        // player.anims.play('left', true);
-    } else if (cursors.right.isDown) {
-        player.setVelocityX(160);
-        // player.anims.play('right', true);
-    } else {
-        player.setVelocityX(0);
-        // player.anims.play('turn');
-    }
-    
-=======
-    this.load.image('sun', 'assets/sun_shiny.png');
+        'assets/dude.png', {
+            frameWidth: 32,
+            frameHeight: 48
+        });
 }
 
 function create() {
     sky = this.add.image(WIDTH/2, HEIGHT/2, 'sky');
     sky.setDisplaySize(WIDTH,HEIGHT);
-    homePlanet = this.add.image(WIDTH / 2, HEIGHT / 2, 'mars');
-    sun = this.add.image(WIDTH / 2, HEIGHT / 2, 'sun');
-    sun_properties.radius = HEIGHT - homePlanet.y;
-    sun_properties.angle = 0;
 
+    earth = this.add.sprite(config.width / 2, config.height / 2, 'earth');
+    earth.scaleX = 0.4;
+    earth.scaleY = 0.4;
+    raio = ((earth.height * earth.scaleY) / 2);
+    player = this.add.sprite(earth.x, earth.y - raio, 'player');
+
+    player.y -= player.height / 2;
+
+    
+    //player.setCollideWorldBounds(true);
+    cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update() {
-    updateSun();
-}
+    // if (cursors.left.isDown) {
+    //     player.setVelocityX(-160);
 
+    //     // player.anims.play('left', true);
+    // } else if (cursors.right.isDown) {
+    //     player.setVelocityX(160);
+    //     // player.anims.play('right', true);
+    // } else {
+    //     player.setVelocityX(0);
+    //     // player.anims.play('turn');
+    // }
 
-function updateSun() {
-    sun.x = homePlanet.x + Math.cos(sun_properties.angle) * sun_properties.radius;
-    sun.y = homePlanet.y + Math.sin(sun_properties.angle) * sun_properties.radius;
-    sun_properties.angle += Math.PI / 512;
-
->>>>>>> b1a52344ce952db4a85eacc1f35ca6e1e317e9a7
 }
