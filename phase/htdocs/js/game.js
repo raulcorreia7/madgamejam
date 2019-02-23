87668e0c8abf93eba925b79e4a5aec439fe77bb0
Rúben Santos <1161391@isep.ipp.pt>
2019-02-23 00:56:20 +0000

    guy rolls

--- phase/htdocs/js/game.js
+++ phase/htdocs/js/game.js
@@ -28,10 +28,22 @@ var game = new Phaser.Game(config);
 
 //The sun
 var sun;
+
+var sun_properties = {
+    radius: 0,
+    angle: 0
+}
 //Earth
 var earth;
 //Sky
 var sky;
+
+// Player
+var player;
+var player_properties = {
+    radius: 0,
+    angle: 0
+};
 var cursors;
 var raio;
 var rotation = 0.3;
@@ -42,6 +54,7 @@ var rotation;
 function preload() {
     this.load.image('sky', 'assets/sky.png');
     this.load.image('earth', ['assets/earth.png','assets/earth_n.png']);
+    this.load.image('sun', 'assets/sun.png');
     this.load.spritesheet('player',
         'assets/dude.png', {
             frameWidth: 32,
@@ -60,23 +73,45 @@ function create() {
     player = this.add.sprite(earth.x, earth.y - raio, 'player');
 
     player.y -= player.height / 2;
+    player_properties.radius = raio + player.height/2;
+    player_properties.angle = 0;
 
-
+    player.x = earth.x + Math.cos(player_properties.angle) * player_properties.radius;
+    player.y = earth.y + Math.sin(player_properties.angle) * player_properties.radius;
+    player.rotation = Math.PI / 2;
+    sun = this.add.image(config.width - 300, 200, 'sun');
+    sun_properties.radius = raio * 2;
+    sun_properties.angle = 0;
     //player.setCollideWorldBounds(true);
     cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update() {
-    // if (cursors.left.isDown) {
-    //     player.setVelocityX(-160);
+    if (cursors.left.isDown) {
+        player.x = earth.x + Math.cos(player_properties.angle) * player_properties.radius;
+        player.y = earth.y + Math.sin(player_properties.angle) * player_properties.radius;
+        player.rotation -= Math.PI / 512;
+        player_properties.angle -= Math.PI / 512;
 
-    //     // player.anims.play('left', true);
-    // } else if (cursors.right.isDown) {
-    //     player.setVelocityX(160);
-    //     // player.anims.play('right', true);
-    // } else {
-    //     player.setVelocityX(0);
-    //     // player.anims.play('turn');
-    // }
 
+        // player.anims.play('left', true);
+    } else if (cursors.right.isDown) {
+        player.x = earth.x + Math.cos(player_properties.angle) * player_properties.radius;
+        player.y = earth.y + Math.sin(player_properties.angle) * player_properties.radius;
+        player.rotation += Math.PI / 512;
+        player_properties.angle += Math.PI / 512;
+    } else {
+        // player.setVelocityX(0);
+        // player.anims.play('turn');
+    }
+    updateSun();
 }
+
+function updateSun(){
+    sun.x = earth.x + Math.cos(sun_properties.angle) * sun_properties.radius;
+    sun.y = earth.y + Math.sin(sun_properties.angle) * sun_properties.radius;
+    sun_properties.angle += Math.PI / 512;
+}
+
+
+
