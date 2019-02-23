1909f1cb7452dd66365b005d052b232bfe844936
Raul Correia <raulcorreia7@gmail.com>
2019-02-23 17:00:07 +0000

    planet health

--- phase/htdocs/js/sun.js
+++ phase/htdocs/js/sun.js
@@ -9,11 +9,12 @@ class Sun {
         this.entity.scaleX = 0.35;
         this.entity.scaleY = 0.35;
         this.ANGLE_STEP = Math.PI / 512;
+        this.velocity = 1;
     }
 
     update(earth) {
-        this.entity.x = earth.x() + Math.cos(this.angle) * this.radius;
-        this.entity.y = earth.y() + Math.sin(this.angle) * this.radius;
+        this.entity.x = earth.x() + Math.cos(this.angle) * this.radius * this.velocity;
+        this.entity.y = earth.y() + Math.sin(this.angle) * this.radius * this.velocity;
         this.angle += this.ANGLE_STEP;
         this.light.x = this.entity.x;
         this.light.y = this.entity.y;
