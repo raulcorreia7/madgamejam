6ebf0f5fcc262e98b8ca0049b7922d382e4a296a
Raul Correia <raulcorreia7@gmail.com>
2019-02-23 22:58:00 +0000

    cooldown nyancat

--- phase/htdocs/js/nyancat.js
+++ phase/htdocs/js/nyancat.js
@@ -12,21 +12,32 @@ class NyanCat {
         this.SCREEN_WIDTH = width;
         this.SCREEN_HEIGHT = height;
         this.COOLDOWN = 1 * 1000;
+        this.DEFAULT_COOLDOWN = 7 * 1000;
+        this.GO_NYAN_BE_FREE = true;
     }
 
 
     update(deltaTime) {
-        this.ElapsedTime += deltaTime;
-        if (this.ElapsedTime >= this.TIME_INTERVAL) {
-            this.SPRITE_INDEX = (this.SPRITE_INDEX % this.MAX_FRAMES) + 1;
-            this.entity.setFrame(this.SPRITE_INDEX);
-            this.ElapsedTime = 0;
+        this.COOLDOWN -= deltaTime;
+        if (this.COOLDOWN <= 0) {
+            this.GO_NYAN_BE_FREE = true;
         }
-        this.entity.x += 10;
-        if (this.entity.x > this.SCREEN_WIDTH + 200) {
-            this.entity.x = -1000;
-            this.entity.y = Phaser.Math.FloatBetween(this.SCREEN_HEIGHT * 0.1, this.SCREEN_HEIGHT * 0.9);
+        if (this.GO_NYAN_BE_FREE) {
+            this.ElapsedTime += deltaTime;
+            if (this.ElapsedTime >= this.TIME_INTERVAL) {
+                this.SPRITE_INDEX = (++this.SPRITE_INDEX % this.MAX_FRAMES);
+                this.entity.setFrame(this.SPRITE_INDEX);
+                this.ElapsedTime = 0;
+            }
+            this.entity.x += 10;
+            if (this.entity.x > this.SCREEN_WIDTH + 200) {
+                this.entity.x = -1000;
+                this.entity.y = Phaser.Math.FloatBetween(this.SCREEN_HEIGHT * 0.1, this.SCREEN_HEIGHT * 0.9);
+                this.COOLDOWN = this.DEFAULT_COOLDOWN;
+                this.GO_NYAN_BE_FREE = false;
+            }
         }
+
     }
 
     setPos(x, y) {
