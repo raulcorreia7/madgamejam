6ca499bda4afed4ac383e37841ced098ddc65219
Raul Correia <raulcorreia7@gmail.com>
2019-02-23 00:59:33 +0000

    refactor

--- phase/htdocs/js/game.js
+++ phase/htdocs/js/game.js
@@ -53,7 +53,7 @@ var rotation;
 
 function preload() {
     this.load.image('sky', 'assets/sky.png');
-    this.load.image('earth', ['assets/earth.png','assets/earth_n.png']);
+    this.load.image('earth', ['assets/earth.png', 'assets/earth_n.png']);
     this.load.image('sun', 'assets/sun.png');
     this.load.spritesheet('player',
         'assets/dude.png', {
@@ -66,23 +66,11 @@ function create() {
     sky = this.add.image(WIDTH / 2, HEIGHT / 2, 'sky');
     sky.setDisplaySize(WIDTH, HEIGHT);
 
-    earth = this.add.sprite(config.width / 2, config.height / 2, 'earth');
-    earth.scaleX = 0.4;
-    earth.scaleY = 0.4;
-    raio = ((earth.height * earth.scaleY) / 2);
-    player = this.add.sprite(earth.x, earth.y - raio, 'player');
+    createEarth();
+    createPlayer();
 
-    player.y -= player.height / 2;
-    player_properties.radius = raio + player.height/2;
-    player_properties.angle = 0;
 
-    player.x = earth.x + Math.cos(player_properties.angle) * player_properties.radius;
-    player.y = earth.y + Math.sin(player_properties.angle) * player_properties.radius;
-    player.rotation = Math.PI / 2;
-    sun = this.add.image(config.width - 300, 200, 'sun');
-    sun_properties.radius = raio * 2;
-    sun_properties.angle = 0;
-    //player.setCollideWorldBounds(true);
+
     cursors = this.input.keyboard.createCursorKeys();
 }
 
@@ -107,11 +95,36 @@ function update() {
     updateSun();
 }
 
-function updateSun(){
+function updateSun() {
     sun.x = earth.x + Math.cos(sun_properties.angle) * sun_properties.radius;
     sun.y = earth.y + Math.sin(sun_properties.angle) * sun_properties.radius;
     sun_properties.angle += Math.PI / 512;
 }
 
+function createEarth() {
+    earth = this.add.sprite(config.width / 2, config.height / 2, 'earth');
+    earth.scaleX = 0.4;
+    earth.scaleY = 0.4;
+    raio = ((earth.height * earth.scaleY) / 2);
 
+}
 
+function createPlayer() {
+    player = this.add.sprite(earth.x, earth.y - raio, 'player');
+
+    player.y -= player.height / 2;
+    player_properties.radius = raio + player.height / 2;
+    player_properties.angle = 0;
+
+    player.x = earth.x + Math.cos(player_properties.angle) * player_properties.radius;
+    player.y = earth.y + Math.sin(player_properties.angle) * player_properties.radius;
+    player.rotation = Math.PI / 2;
+    player.setCollideWorldBounds(true);
+}
+
+function createSun() {
+    sun = this.add.image(config.width - 300, 200, 'sun');
+    sun_properties.radius = raio * 2;
+    sun_properties.angle = 0;
+
+}
