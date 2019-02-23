97384f20b890588220d380c098a903b5cacc3121
Miguel Santos [1161386] <1161386@isep.ipp.pt>
2019-02-23 21:55:40 +0000

    asteroid

--- phase/htdocs/js/game.js
+++ phase/htdocs/js/game.js
@@ -160,9 +160,10 @@ function createPlanets(game) {
 
         etPlanets_physics.add(et.entity);
         etPlanets.push(et);
-        
+
         start += step;
-        planet.addAsteroid(particle);
+        let particle = new Particle(game, et);
+        et.addAsteroid(particle);
     }
 }
 
